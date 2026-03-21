7774fcda68dc5b3a46e266813ef54c77e5cf2b55
Tanay ⌬ <tanayguha07@gmail.com>
2026-03-22 03:22:23 +0530

    Create app/src/main/java/com/sxam/sxamtop/data/local/BookmarkEntity.kt

---
+++ app/src/main/java/com/sxam/sxamtop/data/local/BookmarkEntity.kt
@@ -0,0 +1,15 @@
+package com.sxam.sxamtop.data.local
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "bookmarks")
+data class BookmarkEntity(
+    @PrimaryKey val id: String,
+    val title: String,
+    val description: String,
+    val source: String,
+    val url: String,
+    val category: String,
+    val publishedAt: Long
+)
