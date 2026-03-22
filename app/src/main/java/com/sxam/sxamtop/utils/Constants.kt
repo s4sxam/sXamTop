da2c5b866d5faa0dcdeb0bca32af0aa15151476b
Tanay ⌬ <tanayguha07@gmail.com>
2026-03-22 15:57:23 +0530

    Create app/src/main/java/com/sxam/sxamtop/utils/Constants.kt

---
+++ app/src/main/java/com/sxam/sxamtop/utils/Constants.kt
@@ -0,0 +1,5 @@
+package com.sxam.sxamtop.utils
+object Constants {
+    val NEWS_CATEGORIES = listOf("All", "Top", "World", "Tech", "Sports", "Finance", "User Posts")
+    val POST_CATEGORIES = listOf("World", "Tech", "Sports", "Finance", "Other") // #21 Extracted
+}
