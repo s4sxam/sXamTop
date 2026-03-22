534e7dad8663e26b91131f6dd5182e897fea1c3b
Tanay ⌬ <tanayguha07@gmail.com>
2026-03-22 15:57:48 +0530

    Create app/src/main/java/com/sxam/sxamtop/utils/DateUtils.kt

---
+++ app/src/main/java/com/sxam/sxamtop/utils/DateUtils.kt
@@ -0,0 +1,7 @@
+package com.sxam.sxamtop.utils
+import java.text.SimpleDateFormat
+import java.util.*
+// #22 Extracted from NewsCard.kt
+fun timeAgo(epochMillis: Long): String {
+    // ... implementation logic remains the same
+}
