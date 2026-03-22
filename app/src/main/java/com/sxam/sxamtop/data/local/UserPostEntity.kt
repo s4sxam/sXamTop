d48e1311d9347a92344fde7a069176f262f4cece
Tanay ⌬ <tanayguha07@gmail.com>
2026-03-22 15:36:34 +0530

    Update UserPostEntity.kt

--- app/src/main/java/com/sxam/sxamtop/data/local/UserPostEntity.kt
+++ app/src/main/java/com/sxam/sxamtop/data/local/UserPostEntity.kt
@@ -1,15 +1,16 @@
 package com.sxam.sxamtop.data.local
-
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.UUID
 
 @Entity(tableName = "user_posts")
 data class UserPostEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey val id: String = UUID.randomUUID().toString(), // #30 Use String UUID
     val title: String,
     val description: String,
     val source: String,
     val category: String,
     val url: String,
+    val imageUrl: String?, 
     val createdAt: Long
 )
