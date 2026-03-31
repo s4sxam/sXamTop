5140a0f2e635b891b86a7fdaf899f9ffb1456fbc
Tanay ⌬ <tanayguha07@gmail.com>
2026-03-31 14:19:18 +0530

    Update DetailScreen.kt

--- app/src/main/java/com/sxam/sxamtop/ui/detail/DetailScreen.kt
+++ app/src/main/java/com/sxam/sxamtop/ui/detail/DetailScreen.kt
@@ -6,7 +6,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.AutoMirrored.Filled.ArrowBack
 import androidx.compose.material.icons.filled.OpenInBrowser
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.*
