518a69b8ac7184957e19a7e6b6db7504620dc698
Tanay ⌬ <tanayguha07@gmail.com>
2026-03-31 09:30:11 +0530

    Update NewsApiResponse.kt

--- app/src/main/java/com/sxam/sxamtop/data/remote/NewsApiResponse.kt
+++ app/src/main/java/com/sxam/sxamtop/data/remote/NewsApiResponse.kt
@@ -3,40 +3,18 @@ package com.sxam.sxamtop.data.remote
 import com.google.gson.annotations.SerializedName
 
 data class NewsApiResponse(
-    @SerializedName("status") 
-    val status: String,
-    
-    @SerializedName("totalResults") 
-    val totalResults: Int,
-    
-    @SerializedName("articles") 
-    val articles: List<NewsApiArticle> = emptyList()
+    @SerializedName("totalArticles") val totalArticles: Int = 0,
+    @SerializedName("articles") val articles: List<NewsApiArticle> = emptyList()
 )
 
 data class NewsApiArticle(
-    @SerializedName("title") 
-    val title: String? = "",
-    
-    @SerializedName("description") 
-    val description: String? = "",
-    
-    @SerializedName("source") 
-    val source: NewsApiSource? = null,
-    
-    @SerializedName("publishedAt") 
-    val publishedAt: String? = "",
-    
-    @SerializedName("url") 
-    val url: String? = "",
-    
-    @SerializedName("urlToImage") 
-    val urlToImage: String? = null
+    @SerializedName("title") val title: String = "",
+    @SerializedName("description") val description: String? = "",
+    @SerializedName("source") val source: NewsApiSource = NewsApiSource(),
+    @SerializedName("publishedAt") val publishedAt: String = "",
+    @SerializedName("url") val url: String = ""
 )
 
 data class NewsApiSource(
-    @SerializedName("id") 
-    val id: String? = null,
-    
-    @SerializedName("name") 
-    val name: String? = ""
+    @SerializedName("name") val name: String = ""
 )
